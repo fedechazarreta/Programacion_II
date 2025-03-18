08ada9202ed78e1826b85056a30ede862e1c20cd
fedechazarreta <federico.chazarreta@sagradoalcal.edu.ar>
2025-03-18 16:16:11 -0300

    Clase 18/03

---
+++ Ejercicio2.py
@@ -0,0 +1,4 @@
+float = 10.5
+entero = int(float)
+
+print("float", float, "num int", entero)
---
+++ Ejercicio9.py
@@ -0,0 +1,7 @@
+numero = int(input("Ingrese un numero"))
+if numero > 0 :
+    print("es positivo  .")
+elif numero == 0:
+    print("es cero  .")
+elif numero < 0 :
+    print("es negativo  .")
